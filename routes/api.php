4ab5a224f094b4d4ca2c41e8e8d29756eb778837
imufiid <imufiid98@gmail.com>
2020-12-11 15:26:44 +0700

    try generate qr code

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\LabelAlignment;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Response\QrCodeResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +17,28 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::namespace('Auth')->prefix('auth')->group(function() {
+
+Route::namespace('Auth')->prefix('auth')->group(function () {
     Route::post('register', 'RegisterController');
     Route::post('login', 'LoginController');
     Route::post('logout', 'LogoutController');
+});
+
+Route::get('qrcode', function (Request $request) {
+    $req = $request->get('string');
+    $qrCode = new QrCode($req);
+
+    $qrCode->setSize(500);
+    $qrCode->setMargin(20);
+    
+    // Set advanced options
+    $qrCode->setWriterByName('png');
+    $qrCode->setEncoding('UTF-8');
+    $qrCode->setRoundBlockSize(true, QrCode::ROUND_BLOCK_SIZE_MODE_MARGIN);
+
+    // Save it to a file
+    $path = public_path('img/' . $req . '.png');
+    $qrCode->writeFile($path);
 });
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
