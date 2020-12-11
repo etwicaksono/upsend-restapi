319f75eff8acd432fe44c05dad475c64edd7eb0f
imufiid <imufiid98@gmail.com>
2020-12-11 16:43:31 +0700

    update model

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'firstname', 'lastname', 'email', 'access_id', 'role_id', 'password',
     ];
 
     /**
