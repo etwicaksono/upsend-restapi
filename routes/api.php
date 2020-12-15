fac3c10c51e9bced5758beee3335ce493e244dd8
imufiid <imufiid98@gmail.com>
2020-12-15 20:02:30 +0700

    route api

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Endroid\QrCode\ErrorCorrectionLevel;
-use Endroid\QrCode\LabelAlignment;
-use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Response\QrCodeResponse;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes TA-ITN-UP-Send
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
@@ -24,22 +19,32 @@ Route::namespace('Auth')->prefix('auth')->group(function () {
     Route::post('logout', 'LogoutController');
 });
 
-Route::get('qrcode', function (Request $request) {
-    $req = $request->get('string');
-    $qrCode = new QrCode($req);
+Route::namespace('Event')->prefix('event')->group(function() {
+    // get all event
+    Route::post('/', 'EventController@index');
+    // create event
+    Route::post('create', 'EventController@store');
+    // update event
+    Route::put('update', 'EventController@update');
+    // show event by ID
+    Route::get('show', 'EventController@show');
+    // delete event
+    Route::delete('delete', 'EventController@destroy');
+    // search event
+    Route::get('search', 'EventController@searchEvent');
 
-    $qrCode->setSize(500);
-    $qrCode->setMargin(20);
-    
-    // Set advanced options
-    $qrCode->setWriterByName('png');
-    $qrCode->setEncoding('UTF-8');
-    $qrCode->setRoundBlockSize(true, QrCode::ROUND_BLOCK_SIZE_MODE_MARGIN);
-
-    // Save it to a file
-    $path = public_path('img/' . $req . '.png');
-    $qrCode->writeFile($path);
+    // registration to event
+    Route::post('registration', 'RegisterEventController');
+    // come to event
+    Route::post('joined', 'JoinEventController');
+   
+    Route::prefix('participant')->group(function() {
+        // list of participant registration on event
+        Route::post('/', 'ParticipantController@registration');
+        
+        // list of participant coming on event
+        Route::post('come', 'ParticipantController@come');
+    });
 });
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
+
