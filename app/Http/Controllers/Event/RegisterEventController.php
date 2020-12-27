e1e9e00736624b0924613ef7b95f9f7c5475bb84
imufiid <imufiid98@gmail.com>
2020-12-27 23:36:24 +0700

    check participant in register event

--- app/Http/Controllers/Event/RegisterEventController.php
+++ app/Http/Controllers/Event/RegisterEventController.php
@@ -14,7 +14,7 @@ class RegisterEventController extends Controller
     {
         $this->middleware('auth:api');
     }
-    
+
     /**
      * Handle the incoming request.
      *
@@ -30,25 +30,69 @@ class RegisterEventController extends Controller
         ]);
 
         $codeEvent = request('code_event');
-        $userId = request('user_id');      
+        $userId = request('user_id');
 
         $dataEvent = Event::where('code', $codeEvent)->first();
 
-        if($dataEvent == null) {
+        if ($dataEvent == null) {
             return response()->json([
                 'status' => Response::HTTP_BAD_REQUEST,
                 'message' => 'Event not Found'
             ], Response::HTTP_OK);
         } else {
-            RegisterEvent::create([
-                'user_id' => $userId,
-                'event_id' => $dataEvent->id
-            ]);
-    
-            return response()->json([
-                'status' => Response::HTTP_CREATED,
-                'message' => 'You Register at event '. $dataEvent->name
-            ], Response::HTTP_CREATED);
+
+            // code
+            $checkRegistrationEvent = RegisterEvent::where([
+                "user_id" => $userId,
+                "event_id" => $dataEvent->id
+            ])->first();
+
+            if ($checkRegistrationEvent == null) {
+
+                // goes to registration
+                RegisterEvent::create([
+                    'user_id' => $userId,
+                    'event_id' => $dataEvent->id
+                ]);
+
+                return response()->json([
+                    'status' => Response::HTTP_OK,
+                    'message' => 'You Register at event ' . $dataEvent->name,
+                    'data' => $dataEvent
+                ], Response::HTTP_CREATED);
+            } else {
+
+                $checkIfCome = RegisterEvent::where('user_id', $userId)
+                    ->where('event_id', $dataEvent->id)->first();
+
+                if ($checkIfCome->is_user_come == 1) {
+                    return response()->json([
+                        'status' => Response::HTTP_OK,
+                        'message' => 'Anda Sudah hadir di event ini',
+                    ], Response::HTTP_OK);
+                } else {
+                    // come
+                    $update = RegisterEvent::where('user_id', $userId)
+                        ->where('event_id', $dataEvent->id)
+                        ->update([
+                            'is_user_come' => 1
+                        ]);
+
+                    if ($update > 0) {
+                        return response()->json([
+                            'status' => Response::HTTP_OK,
+                            'message' => 'You Come in Event',
+                            'data' => $checkRegistrationEvent
+                        ], Response::HTTP_OK);
+                    } else {
+                        return response()->json([
+                            'status' => Response::HTTP_BAD_REQUEST,
+                            'message' => 'Bad Request!',
+                            'data' => $checkRegistrationEvent
+                        ], Response::HTTP_OK);
+                    }
+                }
+            }
         }
     }
 }
